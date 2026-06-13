07bd987537275b23904f41f4795bf081d2421d0a
Mosder <xmosder12@gmail.com>
2026-06-13 20:08:47 +0200

    feat: created utils/time.h for helper functions regarding date and time

---
+++ analyzer/utils/time.c
@@ -0,0 +1,10 @@
+#include "time.h"
+
+#include <stdio.h>
+#include <time.h>
+
+void get_date_time_string(char *date_time) {
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    sprintf(date_time, "%02d%02d%02d_%02d%02d%02d", t->tm_year - 100, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+}
